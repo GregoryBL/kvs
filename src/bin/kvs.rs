c04d3b4992fb0d3013730390b0a92b3b36a355a0
Gregory Berns-Leone <gberns.l@gmail.com>
2019-07-02 22:59:13 -0500

    Add in memory store + cmdline arg parsing (#1)
    * Part 1 passing + clippy
    
    * rustfmt

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,73 @@
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+// use kvs::KvStore;
+use std::process;
+
 fn main() {
-    println!("hello, world!");
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("Set a key value pair.")
+                .arg(
+                    Arg::with_name("key")
+                        .help("The name you use to get it out.")
+                        .required(true)
+                        .index(1),
+                )
+                .arg(
+                    Arg::with_name("value")
+                        .help("The value associated with the key.")
+                        .required(true)
+                        .index(2),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("Get a value for a key.")
+                .arg(
+                    Arg::with_name("key")
+                        .help("The name you want the value for.")
+                        .required(true)
+                        .index(1),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("rm").about("Remove a key.").arg(
+                Arg::with_name("key")
+                    .help("The key you want to remove.")
+                    .required(true)
+                    .index(1),
+            ),
+        )
+        .get_matches();
+
+    // let kvs = KvStore::new();
+
+    // let key = matches.value_of("key").unwrap().to_string();
+
+    match matches.subcommand_name() {
+        Some("set") => {
+            // let value = matches.value_of("value").unwrap().to_string();
+            // kvs.set(key,value);
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+        Some("get") => {
+            // kvs.get(key);
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+        Some("rm") => {
+            // kvs.remove(key);
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+        _ => {
+            eprintln!("Error: No subcommand match.");
+            process::exit(1)
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,70 @@
-pub struct KvStore;
+#![deny(missing_docs)]
+
+//! KvStore implementation. A wrapper around HashMap
+
+use std::collections::HashMap;
+use std::string::String;
+
+/// The KvStore type.
+///
+///A wrapper around a HashMap.
+#[derive(Default)]
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
+
 impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore{}
+    /// Create a new KvStore
+    /// ```rust
+    /// use kvs::KvStore;
+    /// let mut store = KvStore::new();
+    /// ```
+    pub fn new() -> Self {
+        KvStore {
+            store: Default::default(),
+        }
     }
 
-    pub fn set(&self, key:String, value:String) {
-        panic!()
+    /// Add a key/value pair
+    ///
+    /// If the key already exists, this will overwrite the value
+    /// already stored for that key.
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut store = KvStore::new();
+    /// store.set("key".to_owned(), "value".to_owned());
+    /// ```
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
     }
 
-    pub fn get(&self, key:String) -> Option<String> {
-        panic!()
+    /// Get the value for a key
+    ///
+    /// If the key exists it will return the value, otherwise
+    /// it will return None
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut store = KvStore::new();
+    /// # store.set("key".to_owned(), "value".to_owned());
+    /// let optVal = store.get("key".to_owned()); // Some(val)
+    /// let optVal2 = store.get("notPresent".to_owned()); // None()
+    /// ```
+    pub fn get(&self, key: String) -> Option<String> {
+        match self.store.get(&key) {
+            Some(value) => Some(value.to_owned()),
+            None => None,
+        }
     }
 
-    pub fn remove(&self, key:String) {
-
+    /// Remove a key/value pair
+    /// ```rust
+    /// # use kvs::KvStore;
+    /// # let mut store = KvStore::new();
+    /// # store.set("key".to_owned(), "value".to_owned());
+    /// store.remove("key".to_owned());
+    /// let val = store.get("key".to_owned()); // None()
+    /// ```
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
     }
 }
